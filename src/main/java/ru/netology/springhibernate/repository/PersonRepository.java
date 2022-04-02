31f96ec69f60da2805252d7dc887904716f352d8
Alexander Lukyanov <lux-nova@mail.ru>
2022-04-02 17:44:00 +0300

    jpa-adv

--- src/main/java/ru/netology/springhibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/springhibernate/repository/PersonRepository.java
@@ -4,6 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.netology.springhibernate.entity.Person;
 import ru.netology.springhibernate.entity.Personality;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Personality> {
+
+    List<Person> findByCityOfLiving(String city);
+
+    List<Person> findByPhysicalEntityAgeLessThan(int age);
+
+    Optional<Person> findByPhysicalEntityNameAndPhysicalEntitySurname(String name, String surname);
 
 }
--- src/main/java/ru/netology/springhibernate/service/DatabaseService.java
+++ src/main/java/ru/netology/springhibernate/service/DatabaseService.java
@@ -3,16 +3,25 @@ package ru.netology.springhibernate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.netology.springhibernate.entity.Person;
-import ru.netology.springhibernate.repository.PersonsRepository;
+import ru.netology.springhibernate.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DatabaseService {
     @Autowired
-    private PersonsRepository personsRepository;
+    private PersonRepository personRepository;
 
     public List<Person> getPersonsByCity(String city) {
-        return personsRepository.getPersonsByCity(city);
+        return personRepository.findByCityOfLiving(city);
+    }
+
+    public List<Person> getYoungerThan(int age) {
+        return personRepository.findByPhysicalEntityAgeLessThan(age);
+    }
+
+    public Optional<Person> getByFullName(String name, String surname) {
+        return personRepository.findByPhysicalEntityNameAndPhysicalEntitySurname(name, surname);
     }
 }
