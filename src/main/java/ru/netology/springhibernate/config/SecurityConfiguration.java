9da9b00fdf8108f5747775e779c25d6d49678e40
Alexander Lukyanov <lux-nova@mail.ru>
2022-04-09 01:31:36 +0300

    in memory done

--- src/main/java/ru/netology/springhibernate/config/SecurityConfiguration.java
+++ src/main/java/ru/netology/springhibernate/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ru.netology.springhibernate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -9,10 +10,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Master").password("{noop}password").authorities("read", "write")
+                .withUser("Master").password("{noop}password").roles("ADMIN", "GUEST")
                 .and()
-                .withUser("Slave").password("{noop}psw").authorities("read");
+                .withUser("Slave").password("{noop}psw").roles("GUEST");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and().authorizeRequests()
+                        .antMatchers("/persons/add")
+                        .permitAll()
+
+                .and().authorizeRequests()
+                        .antMatchers("/persons/delete", "/persons", "/persons/")
+                        .hasAnyRole("ADMIN")
+
+                .and().authorizeRequests()
+                        .anyRequest()
+                        .authenticated();
+    }
 
 }
--- src/main/java/ru/netology/springhibernate/controller/ExceptionsHandler.java
+++ src/main/java/ru/netology/springhibernate/controller/ExceptionsHandler.java
@@ -4,29 +4,41 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.netology.springhibernate.exception.PersonNotFoundException;
 
 @ControllerAdvice
 public class ExceptionsHandler {
+
+    private String getCurrentUserName() {
+        return "Уважаемый " + SecurityContextHolder.getContext().getAuthentication().getName() + "!\n";
+    }
+
     @ExceptionHandler(PersonNotFoundException.class)
     public ResponseEntity<String> PNFHandler(PersonNotFoundException pnfe) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Данные не получены: " + pnfe.getLocalizedMessage());
+                .body(getCurrentUserName() + "Данные не получены: " + pnfe.getLocalizedMessage());
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<String> PNFHandler(EmptyResultDataAccessException erdae) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-                .body("Невозможно обработать запрос: " + erdae.getLocalizedMessage());
+                .body(getCurrentUserName() + "Невозможно обработать запрос: " + erdae.getLocalizedMessage());
     }
 
     @ExceptionHandler(IncorrectResultSizeDataAccessException.class)
     public ResponseEntity<String> PNFHandler(IncorrectResultSizeDataAccessException irsde) {
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body("Неоднозначный запрос: " + irsde.getLocalizedMessage());
+                .body(getCurrentUserName() + "Неоднозначный запрос: " + irsde.getLocalizedMessage());
     }
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> MSRPHandler(MissingServletRequestParameterException msrpe) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(getCurrentUserName() + "Передайте параметры согласно спецификации!\n" + msrpe.getLocalizedMessage());
+    }
 
 }
